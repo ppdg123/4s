461c965e510b18120d2bac69372e8764d4f3c433
Wang <ppdg@foxmail.com>
2016-11-19 20:10:26 +0800

    Update mergeNN.m

--- mergeNN.m
+++ mergeNN.m
@@ -1,7 +1,7 @@
-function nn = mergeNN(lowernn,uppernn)
-    nn = nnsetup([lowernn.size(1), lowernn.size(2), uppernn.size(2)]);
-    nn.W{1} = lowernn.W{1};
-    nn.W{2} = uppernn.W{1};
-    nn.b{1} = lowernn.b{1};
-    nn.b{2} = uppernn.b{1};
+function nn = mergeNN(lower_layer,upper_layer)
+    nn = nnsetup([lower_layer.size(1), lower_layer.size(2), upper_layer.size(2)]);
+    nn.W{1} = lower_layer.W{1};
+    nn.W{2} = upper_layer.W{1};
+    nn.b{1} = lower_layer.b{1};
+    nn.b{2} = upper_layer.b{1};
 end
